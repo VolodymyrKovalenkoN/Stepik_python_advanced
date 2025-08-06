2d7627d268e84d8c8144e9f0644785460cfee410
Volodymyr Kovalenko <severskwladimir@gmail.com>
2025-08-06 21:49:55 +0200

    max-in-halv-diagonal

---
+++ matrix/max-in-halv-diagonal.py
@@ -0,0 +1,8 @@
+def input_matrix(size):
+    return [[int(el) for el in input().split()] for _ in range(size)]
+
+n = int(input())
+matrix = input_matrix(n)
+
+max_element = max(matrix[i][j] for i in range(n) for j in range(i+1))
+print(max_element)
